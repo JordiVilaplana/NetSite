7da4d8980e8399b0dfdcff156cf45295745a6a2b
Jordi Vilaplana <jordivilaplana@outlook.es>
2024-10-05 13:40:05 +0200

    Updated template pages

--- NetSite/Pages/Index.cshtml.cs
+++ NetSite/Pages/Index.cshtml.cs
@@ -1,20 +1,14 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace NetSite.Pages
+namespace NetSite.Pages;
+
+public class IndexModel : PageModel
 {
-    public class IndexModel : PageModel
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(
+        ILogger<IndexModel> logger)
     {
-        private readonly ILogger<IndexModel> _logger;
-
-        public IndexModel(ILogger<IndexModel> logger)
-        {
-            _logger = logger;
-        }
-
-        public void OnGet()
-        {
-
-        }
+        _logger = logger;
     }
 }
